16608f3059916171fac8ba5a15d0cc30c5dd1747
Your Name Here <amyr@amyrhaq.com>
2013-09-18 10:43:30 -0700

    added first div

---
+++ php-in-action.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="php-in-action.css">
+	<title>PHP in Action</title>
+</head>
+<body>
+	<div class='header'>
+		<h1>
+			<?php
+			$welcome = 'Lets get started with PHP!';
+			echo welcome;
+			?>
+		</h1>
+	</div> <!-- end of header -->
+</body>
+</html>
