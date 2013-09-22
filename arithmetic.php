ae892227d34975621cde0f2664f211a88ad8a042
Your Name Here <amyr@amyrhaq.com>
2013-09-22 14:30:52 -0700

    completed lesson 6/13

---
+++ arithmetic.php
@@ -0,0 +1,10 @@
+<!-- On line 8, write the PHP code that will calculate 17 * 123 -->
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+        <!-- Include your PHP code between the <p></p> tags! -->
+        <p><?php echo 17*123; ?></p>   
+	</body>
+</html>
