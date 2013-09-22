1352d590659ad90851d027bfcb43e6df1876f7f6
Your Name Here <amyr@amyrhaq.com>
2013-09-22 16:02:26 -0700

    completed lesson 8/13

---
+++ variables.php
@@ -0,0 +1,10 @@
+<!-- Declare a variable, $myFirstVariable, and set it equal to the number 0.  -->
+<html>
+	<head>
+		<title>Variables</title>
+	</head>
+	<body>
+        <!-- Add your PHP code between the <p></p> tags! -->
+        <p><?php $myFirstVariable = 0; ?></p>
+    </body>
+</html>
