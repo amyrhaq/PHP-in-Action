d8d1a0a52c23174ac1ac63754b11fd7625d78159
Your Name Here <amyr@amyrhaq.com>
2013-09-22 15:28:36 -0700

    completed lesson 7/13

---
+++ strings.php
@@ -0,0 +1,11 @@
+<!-- Go ahead and echo a string of your choice on line 9. 
+Try out the concatenation operator if you're feeling bold! -->
+<html>
+	<head>
+		<title>Strings</title>
+	</head>
+	<body>
+        <!-- Echo your string between the <p></p> tags! -->
+        <p><?php echo "Hello," . " " . "world" . "!"; ?></p>
+	</body>
+</html>
