261f6906f64408dd37cfdaef7da09bb6d7555fb4
Your Name Here <amyr@amyrhaq.com>
2013-09-22 13:38:36 -0700

    completed lesson 4/13

---
+++ a-bit-of-php-history.php
@@ -0,0 +1,15 @@
+<!-- Notice how our main file is now index.php instead of index.html -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A Bit of PHP History</title>
+	</head>
+	<body>
+        <p>
+            <?php echo 40+2; ?>
+        </p>
+        <p>PHP (originally Personal Home Page, but now PHP: Hypertext Preprocessor) was created by Rasmus Lerdorf in 1995. Lerdorf came up with PHP just to maintain his personal home page (hence the name), but it has since grown to become one of the most popular programming languages in use today.</p> 
+        <p>You might have noticed that our main file is now index.php instead of index.html. This tells the PHP interpreter that there's PHP code in the file to evaluate.</p> 
+        <p>PHP syntax might seem a little intimidating at first, but don't worry! It's very simple to learn.</p>   
+	</body>
+</html>
