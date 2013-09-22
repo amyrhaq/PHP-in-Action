fc4eae268d89496fb43771a35850e815261ff6fd
Your Name Here <amyr@amyrhaq.com>
2013-09-22 10:41:11 -0700

    added array of things

--- php-in-action.php
+++ php-in-action.php
@@ -4,6 +4,7 @@
 	<title>PHP in Action</title>
 </head>
 <body>
+	<img src ='http://placehold.it/150x150' />
 	<div class='header'>
 		<h1>
 			<?php
@@ -12,5 +13,28 @@
 			?>
 		</h1>
 	</div> <!-- end of header -->
+		<p>Generate a list:
+			<?php
+			for ($number=1; $number <= 10; $number++) 
+				{ 
+					if ($number <= 9) {
+						echo $number . ", ";
+				}	else {
+						echo $number . "!";
+				}
+			}; ?>
+		</p>
+		<p>Things you can do:
+			<?php 
+				$things = array("talk to databases", "send cookies", "evaluate form data", "build dynamic webpages");
+				foreach ($things as $thing) 
+				{
+				 	echo '<li>$thing</li>';
+				} 
+				unset($thing);
+			?>
+		</p>
+			
+
 </body>
 </html>
