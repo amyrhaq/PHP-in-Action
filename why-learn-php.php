92625073f3120956409dd868850042786888ff8f
Your Name Here <amyr@amyrhaq.com>
2013-09-22 13:20:37 -0700

    completed lesson 2/13

---
+++ why-learn-php.php
@@ -0,0 +1,11 @@
+<!-- We've written a little PHP here but its not complete! 
+Type 'My first line of PHP!' between the ""s -->
+<html>
+<head>
+	<title>Why Learn PHP</title>
+</head>
+<link rel="stylesheet" type="text/css" href="why-learn-php.css">
+<body>
+	<p><?php echo "My first line of PHP!"; ?></p>
+</body>
+</html>
