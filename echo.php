afbec30d5d66f4ea876148b6684addd3988fbdeb
Your Name Here <amyr@amyrhaq.com>
2013-09-22 13:42:45 -0700

    completed lesson 5/13

---
+++ echo.php
@@ -0,0 +1,13 @@
+<!-- Go ahead and write the PHP code to echo "I'm learning PHP!" on line 10. -->
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Hello, World!</title>
+	</head>
+	<body>
+        <!-- Add your PHP code in the tag below! -->
+        <h1><?php
+            echo "I'm learning PHP!";    
+            ?></h1>
+	</body>
+</html>
