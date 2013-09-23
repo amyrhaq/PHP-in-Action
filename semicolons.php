d32990492861150d45d966286e8a3dbcbb035b69
Your Name Here <amyr@amyrhaq.com>
2013-09-22 21:31:19 -0700

    completed lesson 9/13

---
+++ semicolons.php
@@ -0,0 +1,11 @@
+<!-- We've accidentally forgotten our semicolon on line 8. Add it in! -->
+<html>
+	<head>
+		<title>Semicolons</title>
+	</head>
+	<body>
+        <p><?php
+            echo "Oh, the humanity!";
+          ?></p>
+    </body>
+</html>
