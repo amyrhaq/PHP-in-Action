34431820c648f70fee41dbe2ecb12755d27ee2a4
Your Name Here <amyr@amyrhaq.com>
2013-09-22 23:05:14 -0700

    completed lesson 13/13

---
+++ echo-it.php
@@ -0,0 +1,17 @@
+<!-- Beneath your existing PHP code, use echo to print out your name and your age -->
+<html>
+    <head>
+		<title>Echo It!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+        <?php 
+        $myName = "Amyr"; 
+        $myAge = 25;
+        echo $myName;
+        echo $myAge;
+        ?>    
+        </p>   
+	</body>
+</html>
