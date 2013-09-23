ebb034e88737ac34bbc7b374634385cb8938321e
Your Name Here <amyr@amyrhaq.com>
2013-09-22 21:49:32 -0700

    completed lesson 10/13

---
+++ comments.php
@@ -0,0 +1,12 @@
+<!-- Go ahead and add a comment to our PHP code. It can say whatever you like! -->
+<html>
+	<head>
+		<title>Comments</title>
+	</head>
+	<body>
+        <p><?php
+            echo "Oh, the humanity!";
+            //this is a comment
+          ?></p>
+    </body>
+</html>
