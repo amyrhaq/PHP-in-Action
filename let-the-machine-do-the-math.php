a8bb1188c5f0cd630b21065dadcf583760eb8383
Your Name Here <amyr@amyrhaq.com>
2013-09-22 23:01:15 -0700

    completed lesson 12/13

---
+++ let-the-machine-do-the-math.php
@@ -0,0 +1,13 @@
+<!-- After your first variable, declare a second, $myAge, and set it equal to your age as a number. 
+Remember: no quotes around numbers! -->
+<html>
+    <head>
+		<title>Let the machine do the math</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+        <?php $myName = "Amyr"; $myAge = 25; ?>    
+        </p>   
+	</body>
+</html>
