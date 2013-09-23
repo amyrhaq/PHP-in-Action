56f21e7ee18b1a46e0a177ce3556ab3cc2033fe6
Your Name Here <amyr@amyrhaq.com>
2013-09-22 22:30:56 -0700

    completed lesson 11/13

---
+++ creating-a-variable.php
@@ -0,0 +1,12 @@
+<!-- Declare a variable, $myName, and give it your name as a string. -->
+<html>
+    <head>
+		<title>Creating a Variable</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+        <?php $myName = "Amyr"; ?>    
+        </p>   
+	</body>
+</html>
